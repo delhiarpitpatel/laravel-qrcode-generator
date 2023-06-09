30e2795e5bd3fdb9e0d2a30c9afec5e3d485934b
Arpit Patel <delhiarpitpatel@gmail.com>
2023-06-09 15:17:57 +0530

    Added QRCode Library and implemented a demo

---
+++ app/Http/Controllers/QrCodeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class QrCodeController extends Controller
+{
+    public function index()
+    {
+        return view('qrcode');
+    }
+}
---
+++ resources/views/qrcode.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>How to Generate QR Code in Laravel 8</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+
+<body>
+
+    <div class="container mt-4">
+
+        <div class="card">
+            <div class="card-header">
+                <h2>Simple QR Code</h2>
+            </div>
+            <div class="card-body">
+                {!! QrCode::size(300)->generate('The QR Code works!') !!}
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <h2>Color QR Code</h2>
+            </div>
+            <div class="card-body">
+                {!! QrCode::size(300)->backgroundColor(255, 90, 0)->generate('The QR Code works!') !!}
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
